48017a5df7cf14e5eb00b05b66310e860a2f6bb6
derek <frankderekdick@gmail.com>
2020-11-09 11:37:04 +0800

    Adds the port allocation part.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <my-menu v-model="menuSelectedIndex" v-on:selectedIndexChange="onMenuSelectedIndexChange"></my-menu>
 
     <transition name="fade" mode="out-in">
-      <router-view/>
+      <router-view />
     </transition>
   </div>
 </template>
@@ -62,10 +62,23 @@ export default {
   width: 680px;
 }
 
+.content_container > *::selection {
+  background: #89c4f466;
+}
+
 .content__title {
   font-family: "OpenSans";
   font-size: 44px;
   text-align: center;
+  color: #3c4043;
+  font-weight: 400;
+  margin: 12px 0 12px 0;
+}
+
+.content__subtitle {
+  font-family: "OpenSans";
+  font-size: 28px;
+  text-align: start;
   color: #3c4043;
   font-weight: 400;
   margin: 12px 0 12px 0;
--- frontend/src/views/Accounts.vue
+++ frontend/src/views/Accounts.vue
@@ -5,7 +5,72 @@ Component for displaying the information of all the accounts on our servers.
   <div class="accounts SubPage">
     <div class="content_container">
       <h1 class="content__title">Accounts</h1>
-      <p>Under construction...</p>
+
+      <h2 class="content__subtitle">Port allocation</h2>
+
+      <table class="table-fill">
+        <thead>
+        <tr>
+        <th class="text-left">User</th>
+        <th class="text-left">iid-server-1 (10.19.129.132)</th>
+        <th class="text-left">iid-server-2 (10.19.129.133)</th>
+        </tr>
+        </thead>
+
+        <tbody class="table-hover">
+        <tr>
+        <td class="text-left">xin</td>
+        <td class="text-left">41001 - 41100</td>
+        <td class="text-left">None</td>
+        </tr>
+        <tr>
+        <td class="text-left">bian</td>
+        <td class="text-left">41101 - 41200</td>
+        <td class="text-left">None</td>
+        </tr>
+        <tr>
+        <td class="text-left">lengqy</td>
+        <td class="text-left">41201 - 41300</td>
+        <td class="text-left">None</td>
+        </tr>
+        <tr>
+        <td class="text-left">xih</td>
+        <td class="text-left">41301 - 41400</td>
+        <td class="text-left">None</td>
+        </tr>
+        <tr>
+        <td class="text-left">jingye</td>
+        <td class="text-left">41401 - 41500</td>
+        <td class="text-left">None</td>
+        </tr>
+
+        <tr>
+        <td class="text-left">houjf</td>
+        <td class="text-left">41501 - 41600</td>
+        <td class="text-left">41501 - 41600</td>
+        </tr>
+
+        <tr>
+        <td class="text-left">tangyx</td>
+        <td class="text-left">41601 - 41700</td>
+        <td class="text-left">None</td>
+        </tr>
+
+        <tr>
+        <td class="text-left">xiaoxuan</td>
+        <td class="text-left">41701 - 41800</td>
+        <td class="text-left">None</td>
+        </tr>
+
+        <tr>
+        <td class="text-left">wss</td>
+        <td class="text-left">41801 - 41900</td>
+        <td class="text-left">None</td>
+        </tr>
+        </tbody>
+      </table>
+
+      
     </div>
   </div>
 </template>
@@ -23,4 +88,120 @@ export default {
 </script>
 
 <style scoped>
+/** Table Styles **/
+.table-fill {
+  font-family: 'monaco';
+  background: white;
+  border-radius: 3px;
+  border-collapse: collapse;
+  height: 320px;
+  margin: 0 auto 20px auto;
+  max-width: 600px;
+  padding: 5px;
+  width: 100%;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  animation: float 5s infinite;
+}
+ 
+th {
+  color:#D5DDE5;;
+  background:#1b1e24;
+  border-bottom: 4px solid #9ea7af;
+  border-right: 1px solid #343a45;
+  font-size: 16px;
+  font-style: bold;
+  padding: 24px;
+  text-align:left;
+  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+  vertical-align:middle;
+}
+
+th:first-child {
+  border-top-left-radius:3px;
+}
+
+th:last-child {
+  border-top-right-radius:3px;
+  border-right:none;
+}
+
+tr {
+  border-top: 1px solid #C1C3D1;
+  border-bottom: 1px solid #C1C3D1;
+  color:#666B85;
+  font-size: 16px;
+  padding: 10px 24px;
+  font-weight:normal;
+  text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);
+}
+
+tr:hover td {
+  background:#4E5066;
+  color:#FFFFFF;
+  border-top: 1px solid #22262e;
+}
+
+tr:first-child {
+  border-top:none;
+}
+
+tr:last-child {
+  border-bottom:none;
+}
+
+tr:nth-child(odd) td {
+  background:#EBEBEB;
+}
+
+tr:nth-child(odd):hover td {
+  background:#4E5066;
+}
+
+tr:last-child td:first-child {
+  border-bottom-left-radius:3px;
+}
+
+tr:last-child td:last-child {
+  border-bottom-right-radius:3px;
+}
+
+td {
+  background:#FFFFFF;
+  padding: 10px 24px;
+  text-align: left;
+  vertical-align: middle;
+  font-weight: 300;
+  font-size: 1rem;
+  text-shadow: -1px -1px 1px rgba(0, 0, 0, 0.1);
+  border-right: 1px solid #C1C3D1;
+}
+
+td:last-child {
+  border-right: 0px;
+}
+
+th.text-left {
+  text-align: left;
+}
+
+th.text-center {
+  text-align: center;
+}
+
+th.text-right {
+  text-align: right;
+}
+
+td.text-left {
+  text-align: left;
+}
+
+td.text-center {
+  text-align: center;
+}
+
+td.text-right {
+  text-align: right;
+}
+/** Table Styles **/
 </style>
