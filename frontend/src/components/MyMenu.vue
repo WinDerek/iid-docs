c3972b85ba1990a92340e48a872c91c087e0490a
derek <frankderekdick@gmail.com>
2020-11-12 02:22:45 +0800

    Makes responsive layouts.

--- frontend/src/components/MyMenu.vue
+++ frontend/src/components/MyMenu.vue
@@ -62,6 +62,79 @@
         <img src="../assets/images/github_mark_32px.png" style="width: 32px;" />
       </a>
     </div>
+
+    <!-- The menu toggle button -->
+    <div class="menu-item-flex toggle-button-container">
+      <button class="toggle-button" :class="{ 'active': toggleButtonActive }" v-on:click="onToggleButtonClicked()">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </button>
+
+      <nav id="mobileMenu" :class="{ active: toggleButtonActive }">
+        <ul>
+          <li>
+            <router-link
+              to="/home"
+              class="mobile-menu-link-label"
+              :class="{ selected: selectedIndex === 0 }"
+              v-on:click.native="onMobileMenuLinkClicked(0)">
+              <div class="mobile-menu-link-text-container">
+                <div class="mobile-menu-link-text">Home</div>
+              </div>
+            </router-link>
+          </li>
+
+          <li>
+            <router-link
+              to="/home"
+              class="mobile-menu-link-label"
+              :class="{ selected: selectedIndex === 1 }"
+              v-on:click.native="onMobileMenuLinkClicked(1)">
+              <div class="mobile-menu-link-text-container">
+                <div class="mobile-menu-link-text">Accounts</div>
+              </div>
+            </router-link>
+          </li>
+
+          <li>
+            <router-link
+              to="/home"
+              class="mobile-menu-link-label"
+              :class="{ selected: selectedIndex === 2 }"
+              v-on:click.native="onMobileMenuLinkClicked(2)">
+              <div class="mobile-menu-link-text-container">
+                <div class="mobile-menu-link-text">Tutorial</div>
+              </div>
+            </router-link>
+          </li>
+
+          <li>
+            <router-link
+              to="/home"
+              class="mobile-menu-link-label"
+              :class="{ selected: selectedIndex === 3 }"
+              v-on:click.native="onMobileMenuLinkClicked(3)">
+              <div class="mobile-menu-link-text-container">
+                <div class="mobile-menu-link-text">Conferences</div>
+              </div>
+            </router-link>
+          </li>
+
+          <li>
+            <router-link
+              to="/home"
+              class="mobile-menu-link-label"
+              :class="{ selected: selectedIndex === 4 }"
+              v-on:click.native="onMobileMenuLinkClicked(4)">
+              <div class="mobile-menu-link-text-container">
+                <div class="mobile-menu-link-text">Birthdays</div>
+              </div>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -80,11 +153,20 @@ export default {
     }
   },
   data: function() {
-    return {};
+    return {
+      toggleButtonActive: false
+    }
   },
   methods: {
     onLinkClicked(index) {
       this.$emit('selectedIndexChange', index);
+    },
+    onToggleButtonClicked() {
+      this.toggleButtonActive = !this.toggleButtonActive;
+    },
+    onMobileMenuLinkClicked(index) {
+      this.$emit('selectedIndexChange', index);
+      this.onToggleButtonClicked();
     }
   }
 };
@@ -163,6 +245,9 @@ export default {
   position: relative;
 
   overflow: hidden;
+
+  /* visibility: hidden; */
+  display: none;
 }
 
 .link-label:hover {
@@ -211,6 +296,8 @@ export default {
   right: 0;
   bottom: 0;
   padding: 20px;
+
+  display: none;
 }
 
 .github-link-container > a {
@@ -232,4 +319,144 @@ export default {
   user-select: none;
   margin: 4px 4px;
 }
+
+/* Beginning of the styles for the toggle button */
+.toggle-button-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+@media only screen and (min-width: 800px) {
+  .github-link-container {
+    display: flex;
+  }
+
+  .link-label {
+    display: block;
+  }
+  
+  .toggle-button-container {
+    display: none;
+  }
+}
+
+.toggle-button {
+  background: transparent;
+  border: none;
+  text-transform: none;
+  overflow: visible;
+  text-decoration: none;
+  user-select: none;
+  outline: 0;
+
+  /* Larger than #mobileMenu */
+  z-index: 10000;
+}
+
+.toggle-button .line {
+  width: 28px;
+  height: 3px;
+  background-color: #333333;
+  display: block;
+  margin: 6px 0;
+  -webkit-transition: all 0.35s;
+  -o-transition: all 0.35s;
+  transition: all 0.35s;
+}
+
+.toggle-button:hover{
+  cursor: pointer;
+}
+
+.toggle-button.active .line:nth-child(2) {
+  opacity: 0;
+}
+
+.toggle-button.active .line:nth-child(1) {
+  -webkit-transform: translateY(9px) rotate(45deg);
+  -ms-transform: translateY(9px) rotate(45deg);
+  -o-transform: translateY(9px) rotate(45deg);
+  transform: translateY(9px) rotate(-45deg);
+
+  background-color: #eeeeee;
+}
+
+.toggle-button.active .line:nth-child(3) {
+  -webkit-transform: translateY(-9px) rotate(-45deg);
+  -ms-transform: translateY(-9px) rotate(-45deg);
+  -o-transform: translateY(-9px) rotate(-45deg);
+  transform: translateY(-9px) rotate(45deg);
+
+  background-color: #eeeeee;
+}
+/* Ending of the styles for the toggle button */
+
+/* Beginning of the styles for the mobile menu */
+#mobileMenu {
+  display: none;
+  visibility: hidden;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 9999;
+
+  background: #2c82c9ff;
+  opacity: 0;
+
+  transition: all 0.35s ease-in-out;
+}
+
+#mobileMenu.active {
+  display: flex;
+  visibility: visible;
+
+  opacity: 1.0;
+}
+
+#mobileMenu > ul {
+  display: inline;
+  list-style: none;
+  margin: 0;
+}
+
+#mobileMenu > ul > li {
+  margin: 0;
+  padding: 0;
+}
+
+.mobile-menu-link-label {
+  text-decoration: none;
+  margin: 0 18px;
+
+  overflow: hidden;
+}
+
+.mobile-menu-link-text-container {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 20px;
+  font-family: "OpenSans";
+  letter-spacing: 0.25px;
+  font-weight: 600;
+  user-select: none;
+  color: #eeeeee;
+}
+/* Ending of the styles for the mobile menu */
 </style>
