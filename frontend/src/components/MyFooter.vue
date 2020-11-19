b98a8e59ec6395764ff6b7dc1d09c1e05f9365c7
derek <frankderekdick@gmail.com>
2020-11-19 11:19:16 +0800

    Trivial changes.

--- frontend/src/components/MyFooter.vue
+++ frontend/src/components/MyFooter.vue
@@ -36,14 +36,14 @@ export default {
 .my-footer {
   display: flex;
   justify-content: center;
-  align-items: center; 
+  align-items: center;
   margin: 0;
   background: #f5f5f5;
 
   position: absolute;
   bottom: 0;
   width: 100%;
-  height: 280px;
+  height: 260px;
 }
 
 .content__container {
@@ -53,7 +53,7 @@ export default {
   align-items: center; 
   width: 100%;
   margin: auto;
-  padding: 40px;
+  padding: 0;
 }
 
 .logo {
