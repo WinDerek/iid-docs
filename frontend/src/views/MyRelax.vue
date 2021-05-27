7e697df35ef69dfa744ddf403a4eacf178d185c4
derek <frankderekdick@gmail.com>
2021-05-27 17:21:57 +0800

    Adds some websites.

--- frontend/src/views/MyRelax.vue
+++ frontend/src/views/MyRelax.vue
@@ -12,10 +12,46 @@ Component for displaying some websites for fun.
         <a href="https://binaryify.github.io/vue-tetris/" target="_blank">https://binaryify.github.io/vue-tetris/</a>
       </p>
 
-      <h2 class="content__subtitle">免费空调</h2>
+      <h2 class="content__subtitle">免费云空调</h2>
 
       <p>
         <a href="https://ac.yunyoujun.cn/" target="_blank">https://ac.yunyoujun.cn/</a>
+      </p>
+
+      <h2 class="content__subtitle">3D 赛车</h2>
+
+      <p>
+        <a href="https://epicskidmarks.com/" target="_blank">https://epicskidmarks.com/</a>
+      </p>
+
+      <h2 class="content__subtitle">Q 弹马里奥</h2>
+
+      <p>
+        <a href="https://jellymar.io/" target="_blank">https://jellymar.io/</a>
+      </p>
+
+      <h2 class="content__subtitle">Laser Town</h2>
+
+      <p>
+        <a href="http://ludumdare.com/compo/ludum-dare-35/?action=preview&uid=3671" target="_blank">http://ludumdare.com/compo/ludum-dare-35/?action=preview&uid=3671</a>
+      </p>
+
+      <h2 class="content__subtitle">HexGL</h2>
+
+      <p>
+        <a href="http://hexgl.bkcore.com/play/" target="_blank">http://hexgl.bkcore.com/play/</a>
+      </p>
+
+      <h2 class="content__subtitle">2048</h2>
+
+      <p>
+        <a href="http://gabrielecirulli.github.io/2048/" target="_blank">http://gabrielecirulli.github.io/2048/</a>
+      </p>
+
+      <h2 class="content__subtitle">Flappy 2048</h2>
+
+      <p>
+        <a href="http://hczhcz.github.io/Flappy-2048/" target="_blank">http://hczhcz.github.io/Flappy-2048/</a>
       </p>
 
       <h2 class="content__subtitle">Game of Life</h2>
